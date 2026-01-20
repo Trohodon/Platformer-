96a1d01ea81dc4cc4e77f1b7c7279e78f02773b9
Trohodon <isfleshman@gmail.com>
2026-01-20 09:33:26 -0500

    Create powerup_defs.py

---
+++ world/powerup_defs.py
@@ -0,0 +1,68 @@
+# world/powerup_defs.py
+POWERUPS = {
+    "speed": {
+        "name": "Sprint Core",
+        "color": (90, 220, 255),
+        "desc": "+Run speed (stacks, diminishing)",
+    },
+    "hp_up": {
+        "name": "Heart Shard",
+        "color": (255, 90, 120),
+        "desc": "+Max HP (stacks, diminishing)",
+    },
+    "damage": {
+        "name": "Rifle Rune",
+        "color": (255, 200, 80),
+        "desc": "+Bullet damage (stacks, diminishing)",
+    },
+    "armor": {
+        "name": "Plating",
+        "color": (160, 170, 190),
+        "desc": "-Damage taken (stacks, diminishing)",
+    },
+    "frenzy": {
+        "name": "Trigger Relay",
+        "color": (255, 140, 60),
+        "desc": "+Fire rate & small damage (stacks)",
+    },
+    "dash_core": {
+        "name": "Dash Core",
+        "color": (180, 120, 255),
+        "desc": "-Dash CD, +dash speed (4 stacks => +air dash)",
+    },
+    "jump": {
+        "name": "Spring Coil",
+        "color": (120, 255, 140),
+        "desc": "+Jump power (stacks)",
+    },
+    "wing": {
+        "name": "Wing Sigil",
+        "color": (120, 190, 255),
+        "desc": "+Extra jumps at stack thresholds",
+    },
+    "regen": {
+        "name": "Regen Node",
+        "color": (80, 255, 150),
+        "desc": "+HP regen per sec (stacks)",
+    },
+    "spike_resist": {
+        "name": "Spike Insulation",
+        "color": (255, 110, 110),
+        "desc": "-Spike damage (stacks)",
+    },
+    "tenacity": {
+        "name": "Tenacity",
+        "color": (255, 255, 140),
+        "desc": "+Invuln frames after hit (stacks)",
+    },
+    "range": {
+        "name": "Ballistics",
+        "color": (140, 255, 220),
+        "desc": "+Bullet speed (stacks)",
+    },
+    "agility": {
+        "name": "Agility",
+        "color": (170, 255, 170),
+        "desc": "+Air control + small speed (stacks)",
+    },
+}
