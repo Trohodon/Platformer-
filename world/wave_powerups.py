2ce8c1b899951844ef872278318e1a20842b088a
Trohodon <isfleshman@gmail.com>
2026-01-20 09:41:52 -0500

    Update wave_powerups.py

--- world/wave_powerups.py
+++ world/wave_powerups.py
@@ -1,14 +1,14 @@
 # world/wave_powerups.py
-
-# Wave number -> list of powerup IDs that should spawn at wave start
+# Wave number -> list of powerup IDs spawned at wave start
 WAVE_POWERUPS = {
-    1: ["speed"],
-    2: ["hp_up"],
-    3: ["damage"],
-    4: ["dash_core"],
-    5: ["armor", "frenzy"],
-
-    # Example: later you can do multiple drops
-    8: ["hp_up", "damage", "range"],
+    1:  ["speed"],
+    2:  ["hp_up"],
+    3:  ["damage"],
+    4:  ["dash_core"],
+    5:  ["armor", "frenzy"],
+    6:  ["jump"],
+    7:  ["regen"],
+    8:  ["hp_up", "damage", "range"],
+    9:  ["spike_resist"],
     10: ["wing"],
 }
